08478c506aeabac90a217f9747c9ab7362399c35
whendro <wisnu.hendro@gmail.com>
2022-04-14 08:39:51 +0700

    change url

--- health-check.sh
+++ health-check.sh
@@ -59,8 +59,8 @@ done
 if [[ $commit == true ]]
 then
   # Let's make Vijaye the most productive person on GitHub.
-  git config --global user.name 'Vijaye Raji'
-  git config --global user.email 'vijaye@statsig.com'
+  git config --global user.name 'Wisnu Hendro'
+  git config --global user.email 'wisnu.hendro@gmail.com'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
